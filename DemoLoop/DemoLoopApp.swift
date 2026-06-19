1a8d58b9b000d750a2854fe5932e00b6d7cbb755
emoticonyt <doriandelvalle67@gmail.com>
2026-06-19 17:45:30 -0500

    Initial Commit (DemoLoop 1.0)

---
+++ DemoLoop/DemoLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoLoopApp.swift
+//  DemoLoop
+//
+//  Created by Dorian Del Valle on 6/19/26.
+//
+
+import SwiftUI
+
+@main
+struct DemoLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
